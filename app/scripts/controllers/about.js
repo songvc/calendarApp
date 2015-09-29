5871d38a62ada0aa78af2e82826e4ee92d440ce1
songvc@gmail.com <songvc@Vincents-Pro.attlocal.net>
2015-09-28 20:34:53 -0700

    initial yeoman setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calendarAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the calendarAppApp
+ */
+angular.module('calendarAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calendarAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the calendarAppApp
+ */
+angular.module('calendarAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
